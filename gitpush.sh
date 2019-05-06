1930975c1915480f18cced97a30365f8e85df44c
Kim Young Jin <kimyoungjin1001@gmail.com>
2019-05-07 02:09:45 +0800

    made changes

---
+++ gitpush.sh
@@ -0,0 +1,13 @@
+
+cd C:/Users/kimyo/Desktop/Trello-power-up
+# switch to branch you want to use
+git checkout master
+# add all added/modified files
+git add .
+# commit changes
+git commit -m "made changes"
+# push to git remote repository
+git push heroku master
+###########################
+echo Press Enter...
+read
