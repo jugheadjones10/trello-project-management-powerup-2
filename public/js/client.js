d2fd0e4297516d38897d71329f3d36169aec1ef9
Kim Young Jin <kimyoungjin1001@gmail.com>
2019-05-07 16:23:11 +0800

    made changes

--- public/js/client.js
+++ public/js/client.js
@@ -1,7 +1,51 @@
 var Promise = TrelloPowerUp.Promise
 var BLACK_ROCKET_ICON = 'https://cdn.glitch.com/1b42d7fe-bda8-4af8-a6c8-eff0cea9e08a%2Frocket-ship.png?1494946700421'
 
+
+
+
 TrelloPowerUp.initialize({
+    'card-badges': function (t, opts) {
+        t.cards("id").then(function(cardIds){
+            cardIds.forEach(function(id){
+                Trello.get(`cards/${id.id}/list`, function(list){
+                    console.log(list)
+                })
+            })
+        })
+        // return[ 
+        //     dynamic: t.get
+        // ]
+
+        // let cardAttachments = opts.attachments; // Trello passes you the attachments on the card
+        // return t.card('name')
+        //     .get('name')
+        //     .then(function (cardName) {
+        //         console.log('We just loaded the card name for fun: ' + cardName);
+        //         return [{
+        //             // dynamic badges can have their function rerun
+        //             // after a set number of seconds defined by refresh.
+        //             // Minimum of 10 seconds.
+        //             dynamic: function () {
+        //                 // we could also return a Promise that resolves to
+        //                 // this as well if we needed to do something async first
+        //                 return {
+        //                     text: 'Dynamic ' + (Math.random() * 100).toFixed(0).toString(),
+        //                     icon: './images/icon.svg',
+        //                     color: 'green',
+        //                     refresh: 10 // in seconds
+        //                 };
+        //             }
+        //         }, {
+        //             // its best to use static badges unless you need your
+        //             // badges to refresh you can mix and match between
+        //             // static and dynamic
+        //             text: 'Static',
+        //             icon: HYPERDEV_ICON, // for card front badges only
+        //             color: null
+        //         }];
+        //     });
+    },
     'board-buttons': function (t, opts) {
         return [{
             text: 'Myanmar Map',
@@ -59,7 +103,7 @@ TrelloPowerUp.initialize({
                     height: 320
                 }
             }]
-        }else {
+        } else {
             return []
         }
     },
