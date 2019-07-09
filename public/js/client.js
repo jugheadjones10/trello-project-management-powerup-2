9c3d672a801da85f50df0132b84074ff9358593b
Kim Young Jin <kimyoungjin1001@gmail.com>
2019-07-09 16:11:44 +0800

    made changes

--- public/js/client.js
+++ public/js/client.js
@@ -22,21 +22,21 @@ TrelloPowerUp.initialize({
         })
         // Mapping list names to a color in the object listToColorMapper. Object saved to board-level key "listToColorMapper"
 
-        t.board('id').then(function(board){
-            console.log(board)
-            Trello.get(`/boards/${board.id}/actions`, function(actions){
-                console.log(actions)
-                // for(var i = 0; i < actions.length; i++){
-                //     if(actions[i].data.old.due){
-                //         // new date is in actions[i].data.card.due
-                //     }
-                // }
-            }, function(){
-                console.log("error")
-            })
-        })
+        // t.board('id').then(function(board){
+        //     console.log(board)
+        //     Trello.get(`/boards/${board.id}/actions`, function(actions){
+        //         console.log(actions)
+        //         // for(var i = 0; i < actions.length; i++){
+        //         //     if(actions[i].data.old.due){
+        //         //         // new date is in actions[i].data.card.due
+        //         //     }
+        //         // }
+        //     }, function(){
+        //         console.log("error")
+        //     })
+        // })
 
-        // heya()
+        heya()
        
         return [{
             text: 'Myanmar Map',
