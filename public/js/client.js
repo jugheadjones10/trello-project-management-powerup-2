50c45a2a3e094180e5d7708dea932167776dfe9f
Kim Young Jin <kimyoungjin1001@gmail.com>
2019-07-09 16:52:50 +0800

    made changes

--- public/js/client.js
+++ public/js/client.js
@@ -9,7 +9,7 @@ var markerColors = ["yellow", "green", "blue", "purple", "pink", "red"]
 
 
 TrelloPowerUp.initialize({
-    'show-authorization': function(t, options){
+    'show-settings': function(t, options){
         // return what to do when a user clicks the 'Authorize Account' link
         // from the Power-Up gear icon which shows when 'authorization-status'
         // returns { authorized: false }
