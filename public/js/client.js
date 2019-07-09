40df1bbcc3084dc49f0ef11139f10c28a89b882d
Kim Young Jin <kimyoungjin1001@gmail.com>
2019-07-09 15:55:14 +0800

    made changes

--- public/js/client.js
+++ public/js/client.js
@@ -4,7 +4,7 @@ var BLACK_ROCKET_ICON = 'https://cdn.glitch.com/1b42d7fe-bda8-4af8-a6c8-eff0cea9
 //Add a color scheme array
 var markerColors = ["yellow", "green", "blue", "purple", "pink", "red"]
 
-import trelloToGoogle from "./trello-to-googlemaps"
+import trelloToGoogle from "./trello-to-googlemaps.js"
 
 TrelloPowerUp.initialize({
     'board-buttons': function (t, opts) {
