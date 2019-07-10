0b954bb384e074642a0c1dc901529887e88a2764
Kim Young Jin <kimyoungjin1001@gmail.com>
2019-07-11 02:17:10 +0800

    made changes

--- public/js/client.js
+++ public/js/client.js
@@ -168,39 +168,39 @@ TrelloPowerUp.initialize({
         //     });
     },
 
-    'attachment-sections': function (t, options) {
-        // options.entries is a list of the attachments for this card
-        // you can look through them and 'claim' any that you want to
-        // include in your section.
+    // 'attachment-sections': function (t, options) {
+    //     // options.entries is a list of the attachments for this card
+    //     // you can look through them and 'claim' any that you want to
+    //     // include in your section.
 
-        var claimed = options.entries.filter(function (attachment) {
-            return attachment.url.indexOf("https://www.google.com/maps/place/") === 0
-        })
+    //     var claimed = options.entries.filter(function (attachment) {
+    //         return attachment.url.indexOf("https://www.google.com/maps/place/") === 0
+    //     })
 
-        if (claimed && claimed.length > 0) {
-            //Why do I need to check if claimed exists? Isn't checking if its length is larger than 0 already checking if it exists?
+    //     if (claimed && claimed.length > 0) {
+    //         //Why do I need to check if claimed exists? Isn't checking if its length is larger than 0 already checking if it exists?
 
-            // if the title for your section requires a network call or other
-            // potentially lengthy operation you can provide a function for the title
-            // that returns the section title. If you do so, provide a unique id for
-            // your section
-            return [{
-                id: 'Project Location', // optional if you aren't using a function for the title
-                claimed: claimed,
-                icon: BLACK_ROCKET_ICON, // Must be a gray icon, colored icons not allowed.
-                title: "Project Location",
-                content: {
-                    type: 'iframe',
-                    url: t.signUrl('./attachment-section.html', {
-                        arg: 'you can pass your section args here'
-                    }),
-                    height: 320
-                }
-            }]
-        } else {
-            return []
-        }
-    },
+    //         // if the title for your section requires a network call or other
+    //         // potentially lengthy operation you can provide a function for the title
+    //         // that returns the section title. If you do so, provide a unique id for
+    //         // your section
+    //         return [{
+    //             id: 'Project Location', // optional if you aren't using a function for the title
+    //             claimed: claimed,
+    //             icon: BLACK_ROCKET_ICON, // Must be a gray icon, colored icons not allowed.
+    //             title: "Project Location",
+    //             content: {
+    //                 type: 'iframe',
+    //                 url: t.signUrl('./attachment-section.html', {
+    //                     arg: 'you can pass your section args here'
+    //                 }),
+    //                 height: 320
+    //             }
+    //         }]
+    //     } else {
+    //         return []
+    //     }
+    // },
 
     'card-buttons': function (t, options) {
 
