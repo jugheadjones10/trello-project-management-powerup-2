a0ba8afeb29a41d2ff084e2025c978fb24fe6310
Kim Young Jin <kimyoungjin1001@gmail.com>
2019-07-11 01:58:02 +0800

    made changes

--- public/js/client.js
+++ public/js/client.js
@@ -3,6 +3,7 @@ import heya from "./trello-to-googlemaps.js"
 
 var Promise = TrelloPowerUp.Promise
 var BLACK_ROCKET_ICON = 'https://cdn.glitch.com/1b42d7fe-bda8-4af8-a6c8-eff0cea9e08a%2Frocket-ship.png?1494946700421'
+var GRAY_ICON = 'https://cdn.hyperdev.com/us-east-1%3A3d31b21c-01a0-4da2-8827-4bc6e88b7618%2Ficon-gray.svg'
 
 //Add a color scheme array
 var markerColors = ["yellow", "green", "blue", "purple", "pink", "red"]
@@ -49,54 +50,65 @@ TrelloPowerUp.initialize({
         // })
 
         return [
-        {
-            text: "trello-to-google",
-            callback: function(t, options){
-                t.board('id').then(function (board) {
-                    window.boardId = board.id
-                })
-                window.open("../myanmar-calendar.html", "trelloToGoogle", "width=300px,height=100px,resizable")
+            {
+                text: "trello-to-google",
+                callback: function (t, options) {
+                    t.board('id').then(function (board) {
+                        window.boardId = board.id
+                    })
+                    window.open("../myanmar-calendar.html", "trelloToGoogle", "width=300px,height=100px,resizable")
+                },
+                condition: 'edit'
             },
-            condition: 'edit'
-        },
-        {
-            text: 'Myanmar Map',
-            callback: function (t, options) {
-                // var boardName
-                // var boardShortLink
-                // t.board("shorLink", "name").then(function(stuff){
-                //   boardName = stuff.name
-                //   boardShortLink = stuff.shortLink
-                // })
+            {
+                text: 'Myanmar Map',
+                callback: function (t, options) {
+                    // var boardName
+                    // var boardShortLink
+                    // t.board("shorLink", "name").then(function(stuff){
+                    //   boardName = stuff.name
+                    //   boardShortLink = stuff.shortLink
+                    // })
 
-                //var urlTo = "https://trello.com/b/" + boardID + "/" + boardName 
-                // t.navigate({
-                //   url: "https://trello.com/c/U1VQSrJX/16-myanmar-map"
-                // });
+                    //var urlTo = "https://trello.com/b/" + boardID + "/" + boardName 
+                    // t.navigate({
+                    //   url: "https://trello.com/c/U1VQSrJX/16-myanmar-map"
+                    // });
 
-                t.modal({
-                    url: './myanmar-map.html',
-                    accentColor: '#F2D600',
-                    fullscreen: true,
-                    callback: () => console.log('Goodbye.'),
-                    title: "Project Location Overview",
-                })
+                    t.modal({
+                        url: './myanmar-map.html',
+                        accentColor: '#F2D600',
+                        fullscreen: true,
+                        callback: () => console.log('Goodbye.'),
+                        title: "Project Location Overview",
+                    })
+                },
+                condition: 'edit'
             },
-            condition: 'edit'
-        },
-        {
-            text: 'Calendar',
-            callback: function (t, options) {
-                t.modal({
-                    url: './myanmar-calendar-new.html',
-                    accentColor: '#F2D600',
-                    fullscreen: true,
-                    callback: () => console.log('Goodbye.'),
-                    title: "Project timelines overview",
-                })
-            },
-            condition: 'edit'
-        }]
+            {
+                text: 'Calendar',
+                callback: function (t, options) {
+                    t.modal({
+                        url: './myanmar-calendar-new.html',
+                        accentColor: '#F2D600',
+                        fullscreen: true,
+                        callback: () => console.log('Goodbye.'),
+                        title: "Project timelines overview",
+                    })
+                },
+                condition: 'edit'
+            }]
+    },
+    'card-back-section': function (t, options) {
+        return {
+            title: 'Costs recording',
+            icon: GRAY_ICON, // Must be a gray icon, colored icons not allowed.
+            content: {
+                type: 'iframe',
+                url: t.signUrl('../costs-recording.html'),
+                height: 500
+            }
+        };
     },
 
     'card-badges': function (t, opts) {
