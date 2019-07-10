f711312270189a72ce437c109e2f54577127433a
Kim Young Jin <kimyoungjin1001@gmail.com>
2019-07-11 02:14:56 +0800

    made changes

--- public/js/client.js
+++ public/js/client.js
@@ -1,6 +1,3 @@
-
-import heya from "./trello-to-googlemaps.js"
-
 var Promise = TrelloPowerUp.Promise
 var BLACK_ROCKET_ICON = 'https://cdn.glitch.com/1b42d7fe-bda8-4af8-a6c8-eff0cea9e08a%2Frocket-ship.png?1494946700421'
 var GRAY_ICON = 'https://cdn.hyperdev.com/us-east-1%3A3d31b21c-01a0-4da2-8827-4bc6e88b7618%2Ficon-gray.svg'
@@ -21,6 +18,18 @@ TrelloPowerUp.initialize({
     //         fullscreen: true
     //     })
     // },
+
+    'card-back-section': function (t, options) {
+        return {
+            title: 'Costs recording',
+            icon: GRAY_ICON, // Must be a gray icon, colored icons not allowed.
+            content: {
+                type: 'iframe',
+                url: t.signUrl('../costs-recording.html'),
+                height: 230
+            }
+        }
+    },
 
     'board-buttons': function (t, opts) {
 
@@ -98,17 +107,6 @@ TrelloPowerUp.initialize({
                 },
                 condition: 'edit'
             }]
-    },
-    'card-back-section': function (t, options) {
-        return {
-            title: 'Costs recording',
-            icon: GRAY_ICON, // Must be a gray icon, colored icons not allowed.
-            content: {
-                type: 'iframe',
-                url: t.signUrl('../costs-recording.html'),
-                height: 500
-            }
-        }
     },
 
     'card-badges': function (t, opts) {
