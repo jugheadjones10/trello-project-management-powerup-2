47bb419f1d3dc94378c0b8a4a255f3738f992804
Kim Young Jin <kimyoungjin1001@gmail.com>
2019-08-18 20:42:22 +0800

    made changes

--- mongodbprac.js
+++ mongodbprac.js
@@ -1,16 +1,24 @@
+
+// const MongoClient = require('mongodb').MongoClient;
+// const uri = "mongodb+srv://jugheadjones:jugheadjones10@fuck-xdurj.gcp.mongodb.net/test?retryWrites=true&w=majority";
+// const client = new MongoClient(uri, { useNewUrlParser: true });
+// client.connect(err => {
+//     const collection = client.db("test").collection("devices");
+//     console.log("fuck")
+//     // perform actions on the collection object
+//     client.close();
+// });
+
+
 const MongoClient = require('mongodb').MongoClient
-const uri = "mongodb+srv://jugheadjones:jugheadjones10@cluster0-bwizb.gcp.mongodb.net/test?retryWrites=true&w=majority"
+const uri = "mongodb+srv://jugheadjones:jugheadjones10@fuck-xdurj.gcp.mongodb.net/test?retryWrites=true&w=majority"
 const client = new MongoClient(uri, { useNewUrlParser: true })
 client.connect(err => {
-    const db = client.db("test")
-
-    db.collection('inventory').insertOne({
-        item: "canvas",
-        qty: 100,
-        tags: ["cotton"],
-        size: { h: 28, w: 35.5, uom: "cm" }
+    const db = client.db("testme")
+    db.collection("devices").insertOne({ awd: 6060 }).then(function(e){
+        console.log("hey babe")
     })
-
-    console.log("connected mthfker")
+    console.log("ucsas")
+    // perform actions on the collection object
     client.close()
-})
+});
